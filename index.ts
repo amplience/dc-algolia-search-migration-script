0dca561a2963edf54afe722e5d7bf726a44bf2ff
Ben Pearey <bpearey@amplience.com>
2024-08-12 15:20:59 +0100

    feat: reducing log bloat during migration

--- index.ts
+++ index.ts
@@ -45,7 +45,6 @@ if (
 
 logger.info("Creating Algolia CLI profile for DC Algolia");
 await $`algolia profile add --name ${DC_ALGOLIA_PROFILE_NAME} --app-id ${DC_ALGOLIA_INDEX_APP_ID} --api-key ${DC_ALGOLIA_INDEX_API_KEY}`;
-
 logger.info("Creating Algolia CLI profile for Reseller");
 await $`algolia profile add --name ${RESELLER_ALGOLIA_PROFILE_NAME} --app-id ${RESELLER_ALGOLIA_INDEX_APP_ID} --api-key ${RESELLER_ALGOLIA_INDEX_API_KEY}`;
 
@@ -81,39 +80,31 @@ try {
 
     logger.info("Migrating primary indices");
     for (const index of primaries) {
-      logger.info("Migrating primary index", { index });
-      logger.info(`Exporting index objects`, { index });
+      logger.info(`Migrating index objects`, { index });
       await $`algolia objects browse ${index} -p ${DC_ALGOLIA_PROFILE_NAME} > ${TMP_PATH}${index}_objects_export.ndjson`;
-      logger.info(`Importing index objects`, { index });
       await $`algolia objects import ${index} -p ${RESELLER_ALGOLIA_PROFILE_NAME} -F ${TMP_PATH}${index}_objects_export.ndjson`;
-      logger.info(`Exporting index rules`, { index });
+      logger.info(`Migrating index rules`, { index });
       await $`algolia rules browse ${index} -p ${DC_ALGOLIA_PROFILE_NAME} > ${TMP_PATH}${index}_rules_export.ndjson`;
-      logger.info(`Importing index rules`, { index });
       await $`algolia rules import ${index} -p ${RESELLER_ALGOLIA_PROFILE_NAME} -F ${TMP_PATH}${index}_rules_export.ndjson`;
-      logger.info(`Exporting index synonyms`, { index });
+      logger.info(`Migrating index synonyms`, { index });
       await $`algolia synonyms browse ${index} -p ${DC_ALGOLIA_PROFILE_NAME} > ${TMP_PATH}${index}_synonyms_export.ndjson`;
-      logger.info(`Importing index synonyms`, { index });
       await $`algolia synonyms import ${index} -p ${RESELLER_ALGOLIA_PROFILE_NAME} -F ${TMP_PATH}${index}_synonyms_export.ndjson`;
-      logger.info(`Exporting index settings`, { index });
+      logger.info(`Migrating index settings`, { index });
       await $`algolia settings get ${index} -p ${DC_ALGOLIA_PROFILE_NAME} > ${TMP_PATH}${index}_settings_export.ndjson`;
-      logger.info(`Importing index settings`, { index });
       await $`algolia settings import ${index} -p ${RESELLER_ALGOLIA_PROFILE_NAME} -F ${TMP_PATH}${index}_settings_export.ndjson`;
       logger.info("Done migrating primary index", { index });
     }
 
     logger.info("Migrating replica indices");
     for (const replica of replicas) {
-      logger.info("Migrating replica index", { replica });
+      logger.info(`Migrating replica rules`, { replica });
       await $`algolia rules browse ${replica} -p ${DC_ALGOLIA_PROFILE_NAME} > ${TMP_PATH}${replica}_rules_export.ndjson`;
-      logger.info(`Importing index rules`, { replica });
       await $`algolia rules import ${replica} -p ${RESELLER_ALGOLIA_PROFILE_NAME} -F ${TMP_PATH}${replica}_rules_export.ndjson`;
-      logger.info(`Exporting index synonyms`, { replica });
+      logger.info(`Migrating index synonyms`, { replica });
       await $`algolia synonyms browse ${replica} -p ${DC_ALGOLIA_PROFILE_NAME} > ${TMP_PATH}${replica}_synonyms_export.ndjson`;
-      logger.info(`Importing index synonyms`, { replica });
       await $`algolia synonyms import ${replica} -p ${RESELLER_ALGOLIA_PROFILE_NAME} -F ${TMP_PATH}${replica}_synonyms_export.ndjson`;
-      logger.info(`Exporting index settings`, { replica });
+      logger.info(`Migrating index settings`, { replica });
       await $`algolia settings get ${replica} -p ${DC_ALGOLIA_PROFILE_NAME} > ${TMP_PATH}${replica}_settings_export.ndjson`;
-      logger.info(`Importing index settings`, { replica });
       await $`algolia settings import ${replica} -p ${RESELLER_ALGOLIA_PROFILE_NAME} -F ${TMP_PATH}${replica}_settings_export.ndjson`;
       logger.info("Done migrating replica index", { replica });
     }
@@ -123,7 +114,7 @@ try {
 } catch (error) {
   logger.error("Migration failed", { error });
 } finally {
-  logger.info("Removing Algolia CLI profiles");
+  logger.info("Cleaning up Algolia CLI profiles");
   await $`algolia profile remove ${DC_ALGOLIA_PROFILE_NAME}`;
   await $`algolia profile remove ${RESELLER_ALGOLIA_PROFILE_NAME}`;
   logger.info("Migration script complete");
